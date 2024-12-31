ac6566874596380548468c3356062994e2669d07
Daniel Willett <dannywill8@gmail.com>
2024-12-31 06:37:48 -0500

    Fixed location name overlay renderer.

--- UncreatedZoneEditor/Cartography/LocationNameCartographyOverlay.cs
+++ UncreatedZoneEditor/Cartography/LocationNameCartographyOverlay.cs
@@ -1,10 +1,11 @@
-﻿#if CLIENT
+#if CLIENT
 using DevkitServer.API.Cartography;
 using DevkitServer.API.Cartography.Compositors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text.Json;
 using UnityEngine.UI;
 
 namespace Uncreated.ZoneEditor.Cartography;
@@ -13,7 +14,7 @@ public class LocationNameCartographyOverlay : ICartographyCompositor
     public bool SupportsChart => true;
     public bool SupportsSatellite => true;
 
-    public bool Composite(in CartographyCaptureData data, Lazy<RenderTexture> texture, bool isExplicitlyDefined)
+    public bool Composite(in CartographyCaptureData data, Lazy<RenderTexture> texture, bool isExplicitlyDefined, JsonElement config)
     {
         List<LocationDevkitNode> nodes = LocationDevkitNodeSystem.Get().GetAllNodes().ToList();
         if (nodes.Count == 0)
@@ -21,29 +22,49 @@ public class LocationNameCartographyOverlay : ICartographyCompositor
 
         RenderTexture rt = texture.Value;
 
-        GameObject uiObject = new GameObject("Cartography Canvas");
-
         GameObject cameraObject = new GameObject("Cartography Camera");
 
         Camera camera = cameraObject.AddComponent<Camera>();
+        cameraObject.transform.SetPositionAndRotation(CartographyTool.CaptureBounds.center with
+        {
+            y = CartographyTool.LegacyMapping ? 1028f : CartographyTool.CaptureBounds.max.y
+        }, CartographyTool.TransformMatrix.rotation);
+        cameraObject.transform.localScale = Vector3.one;
+
         camera.orthographic = true;
-        camera.aspect = (float)data.ImageSize.x / data.ImageSize.y;
-        camera.orthographicSize = data.ImageSize.y / 2f;
-        //camera.transform.SetPositionAndRotation(CartographyTool.CaptureBounds.center with
-        //{
-        //    y = CartographyTool.LegacyMapping ? 1028f : CartographyTool.CaptureBounds.max.y
-        //}, CartographyTool.TransformMatrix.rotation);
+        camera.nearClipPlane = 0.3f;
+        camera.farClipPlane = 10000f;
+        camera.aspect = CartographyTool.CaptureSize.x / CartographyTool.CaptureSize.y;
+        camera.orthographicSize = CartographyTool.CaptureSize.y * 0.5f;
 
         // hide other layers
         camera.cullingMask = RayMasks.UI;
         camera.clearFlags = CameraClearFlags.Depth;
 
+        GameObject uiObject = new GameObject("Cartography Canvas")
+        {
+            layer = LayerMasks.UI,
+            tag = "UI"
+        };
+
         Canvas canvas = uiObject.AddComponent<Canvas>();
         CanvasScaler scaler = uiObject.AddComponent<CanvasScaler>();
-        canvas.renderMode = RenderMode.ScreenSpaceCamera;
         canvas.worldCamera = camera;
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        canvas.planeDistance = 0.31f;
+        canvas.additionalShaderChannels = AdditionalCanvasShaderChannels.Normal |
+                                          AdditionalCanvasShaderChannels.Tangent |
+                                          AdditionalCanvasShaderChannels.TexCoord1;
         scaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
-        scaler.scaleFactor = 1.0f;
+
+        if (config.ValueKind != JsonValueKind.Undefined && config.TryGetProperty("scale", out JsonElement scaleElement) && scaleElement.ValueKind == JsonValueKind.Number && scaleElement.TryGetSingle(out float scale) && float.IsFinite(scale))
+        {
+            scaler.scaleFactor = scale;
+        }
+        else
+        {
+            scaler.scaleFactor = 1.5f;
+        }
 
         Local? localization = Level.info?.getLocalization();
         foreach (LocationDevkitNode node in nodes)
@@ -56,30 +77,24 @@ public class LocationNameCartographyOverlay : ICartographyCompositor
             if (localization != null && localization.has(key))
                 locName = localization.format(key);
 
-            Vector2 mapLocation = CartographyTool.WorldCoordsToMapCoords(node.transform.position);
+            Vector3 worldPos = node.transform.position;
+            Vector2 mapLocation = CartographyTool.WorldCoordsToMapCoords(worldPos);
 
             // create location uGUI label
-            Type labelType = Type.GetType("SDG.Unturned.GlazierLabel_uGUI, Assembly-CSharp", throwOnError: true)!;
 
-            ISleekLabel label = (ISleekLabel)Activator.CreateInstance(labelType, [ Glazier.Get() ]);
-            labelType.GetMethod("ConstructNew", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy)!.Invoke(label, Array.Empty<object>());
-            labelType.GetMethod("SynchronizeColors", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy)!.Invoke(label, Array.Empty<object>());
-
-            GameObject textObj = (GameObject)labelType.GetProperty("gameObject", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy)!.GetValue(label);
-            textObj.SetLayerRecursively(LayerMasks.UI);
-
-            RectTransform rectTransform = textObj.GetRectTransform();
-            rectTransform.SetParent(canvas.transform);
+            ISleekLabel label = CreateLabel(canvas);
 
             label.PositionOffset_X = -200f;
             label.PositionOffset_Y = -30f;
             label.PositionScale_X = mapLocation.x / data.ImageSize.x;
-            label.PositionScale_Y = mapLocation.y / data.ImageSize.y;
+            label.PositionScale_Y = 1f - mapLocation.y / data.ImageSize.y;
             label.SizeOffset_X = 400f;
             label.SizeOffset_Y = 60f;
             label.Text = locName;
             label.TextColor = ESleekTint.FONT;
             label.TextContrastContext = ETextContrastContext.ColorfulBackdrop;
+
+            label.Update();
         }
 
         camera.targetTexture = rt;
@@ -89,6 +104,27 @@ public class LocationNameCartographyOverlay : ICartographyCompositor
         Object.Destroy(uiObject);
 
         return true;
+    }
+
+    private static readonly Type _labelType = Type.GetType("SDG.Unturned.GlazierLabel_uGUI, Assembly-CSharp", throwOnError: true)!;
+    private static readonly MethodInfo _mtdConstructNew = _labelType.GetMethod("ConstructNew", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy)!;
+    private static readonly MethodInfo _mtdSynchronizeColors = _labelType.GetMethod("SynchronizeColors", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy)!;
+    private static readonly MethodInfo _propGameObject = _labelType.GetProperty("gameObject", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy)!.GetMethod;
+    private ISleekLabel CreateLabel(Canvas parentCanvas)
+    {
+        ISleekLabel label = (ISleekLabel)Activator.CreateInstance(_labelType, Glazier.Get());
+
+        _mtdConstructNew.Invoke(label, Array.Empty<object>());
+        _mtdSynchronizeColors.Invoke(label, Array.Empty<object>());
+
+        GameObject textObj = (GameObject)_propGameObject.Invoke(label, Array.Empty<object>());
+
+        RectTransform rectTransform = textObj.GetRectTransform();
+        rectTransform.SetParent(parentCanvas.transform);
+        rectTransform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+        rectTransform.localScale = Vector3.one;
+
+        return label;
     }
 }
 #endif
